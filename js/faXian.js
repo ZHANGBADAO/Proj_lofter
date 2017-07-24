7aec4cb60693fb701c68897b05dea13add839770
Snap <1039146267@qq.com>
2017-07-24 17:39:22 +0800

    0720

---
+++ js/faXian.js
@@ -0,0 +1,22 @@
+//轮播图------------------------------------
+
+var nav = new Swiper('.nav', {
+    slidesPerView : 'auto',
+    slidesOffsetAfter : 430,
+});
+
+
+var container = new Swiper('.container', {
+
+});
+/*
+
+$("#nav1").on("click",function () {
+    mySwiper1.slideTo(0, 300, false);//切换到第一个slide，速度为1秒
+});
+$("#nav2").on("click",function () {
+    mySwiper1.slideTo(1, 300, false);//切换到第一个slide，速度为1秒
+});
+$("#nav3").on("click",function () {
+    mySwiper1.slideTo(2, 300, false);//切换到第一个slide，速度为1秒
+});*/
